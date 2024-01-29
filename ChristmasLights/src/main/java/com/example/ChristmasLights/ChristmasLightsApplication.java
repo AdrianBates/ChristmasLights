56efdc12d86a619d3b334a2195949e05234983fe
a_bat <a_bat@DESKTOP-FVKPPEM>
2024-01-29 21:48:38 +0000

    Initial check in

---
+++ ChristmasLights/src/main/java/com/example/ChristmasLights/ChristmasLightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChristmasLights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasLightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasLightsApplication.class, args);
+	}
+
+}
